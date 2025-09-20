f0ba5ed4f180e079bb1540a390e6762159c7008f
Ravi teja <ravitejakandul@gmail.com>
2025-09-20 12:00:48 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
+  baseURL:
+    process.env.REACT_APP_API_URL || "http://localhost:5000/api", 
 });
 
 // Automatically attach JWT token
 API.interceptors.request.use((req) => {
-  const token = sessionStorage.getItem("token");
+  const token = localStorage.getItem("token");
   if (token) {
     req.headers = req.headers || {};
     req.headers.Authorization = `Bearer ${token}`;
